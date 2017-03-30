51199cf52db85c947eddc7789a2e1821c7795556
Derek van Vliet <derek@getsetgames.com>
2017-03-30 16:33:46 -0400

    [4.15] fixed header order errors

--- Source/BlueprintSound/Private/BackgroundSoundLifecycleComponent.cpp
+++ Source/BlueprintSound/Private/BackgroundSoundLifecycleComponent.cpp
@@ -3,6 +3,7 @@
 //  Copyright (c) 2015 Get Set Games Inc. All rights reserved.
 //
 
+#include "BackgroundSoundLifecycleComponent.h"
 #include "BlueprintSoundPrivatePCH.h"
 
 void UBackgroundSoundLifecycleComponent::OnRegister()
--- Source/BlueprintSound/Private/BlueprintSoundFunctions.cpp
+++ Source/BlueprintSound/Private/BlueprintSoundFunctions.cpp
@@ -3,6 +3,7 @@
 //  Copyright (c) 2015 Get Set Games Inc. All rights reserved.
 //
 
+#include "BlueprintSoundFunctions.h"
 #include "BlueprintSoundPrivatePCH.h"
 
 #if PLATFORM_IOS
